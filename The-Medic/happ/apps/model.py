7a6e0c043b1a66f8f9cce9bc39b7408c9318c3ff
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-05-07 17:20:17 +0200

    g

--- The-Medic/happ/apps/model.py
+++ The-Medic/happ/apps/model.py
@@ -60,127 +60,90 @@ def app():
     # Prepo
     cat_vars  = ['sex','cp','fbs','restecg','exang','slope','ca','thal']
     num_vars  = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak']
+    num_vars_wfe = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak','chol_fbs']
     
     num_4_tree = pipeline.Pipeline(steps=[
-    ('imputer', impute.SimpleImputer(strategy='mean', add_indicator=False)),])
-
-    cat_4_tree = pipeline.Pipeline(steps=[
-        ('imputer', impute.SimpleImputer(strategy='constant',)),
-        #('onehot', preprocessing.OneHotEncoder(handle_unknown='ignore'))
+    ('imputer', impute.SimpleImputer(strategy='mean', add_indicator=False)),
     ])
 
-    tree_prepro = compose.ColumnTransformer(transformers=[
-        ('num', num_4_tree, num_vars),
-        ('cat', cat_4_tree, cat_vars),
-    ], remainder='drop')
-
+    cat_4_tree = pipeline.Pipeline(steps=[
+    ('imputer', impute.SimpleImputer(strategy='constant')),
+        #('onehot', preprocessing.OneHotEncoder(handle_unknown='ignore'))
+    ])
 
 
     num_4_mult = pipeline.Pipeline(steps=[
-        ('imputer', impute.SimpleImputer(strategy='mean', add_indicator=True)), # mean, median
-        ('scaler', preprocessing.StandardScaler())
+    ('imputer', impute.SimpleImputer(strategy='mean', add_indicator=True)), # mean, median
+    ('scaler', preprocessing.StandardScaler())
     ])
 
     cat_4_mult = pipeline.Pipeline(steps=[
-        ('imputer', impute.SimpleImputer(strategy='constant')),
+    ('imputer', impute.SimpleImputer(strategy='constant')),
         #('onehot', preprocessing.OneHotEncoder(handle_unknown='ignore'))
     ])
 
-    mult_prepro = compose.ColumnTransformer(transformers=[
-        ('num', num_4_mult, num_vars),
-        ('cat', cat_4_mult, cat_vars),
+    #Preprocessors WITH engineered feature
+
+    tree_prepro_wfe = compose.ColumnTransformer(transformers=[
+        ('num', num_4_tree, num_vars_wfe),
+        ('cat', cat_4_tree, cat_vars)
+    ], remainder='drop')
+
+    mult_prepro_wfe = compose.ColumnTransformer(transformers=[
+        ('num', num_4_mult, num_vars_wfe),
+        ('cat', cat_4_mult, cat_vars)
     ], remainder='drop') 
 
+    mult_models = {
+    "LogRegr":        LogisticRegression(),
+    "Ridge":          RidgeClassifier(),
+    "SVM_lin":         SVC(kernel="linear", probability=True), # C=0.025
+    "SVM_rbf":         SVC(kernel='rbf', probability=True),
+    "NuSVC":           NuSVC(probability=True),
+    "LinearSVC":       LinearSVC(),
+    "KNN3":            KNeighborsClassifier(3),
+    "KNN5":            KNeighborsClassifier(5),
+    "GaussianNB":      GaussianNB()
+        }
 
-
-    # Model building
-    
-    mult_classifiers = {
-  "LogRegr":        LogisticRegression(),
-  "Ridge":          RidgeClassifier(),
-  "SVM_lin":         SVC(kernel="linear", probability=True), # C=0.025
-  "SVM_rbf":         SVC(kernel='rbf', probability=True),
-  "NuSVC":           NuSVC(probability=True),
-  "LinearSVC":       LinearSVC(),
-  "KNN3":            KNeighborsClassifier(3),
-  "KNN5":            KNeighborsClassifier(5),
-  "GaussianNB":      GaussianNB()
-}
-
-    tree_classifiers = {
+    tree_models = {
     "Decision Tree": DecisionTreeClassifier(),
     "Extra Trees": ExtraTreesClassifier(),
     "Random Forest": RandomForestClassifier(),
     "AdaBoost": AdaBoostClassifier(),
     "Skl GBM": GradientBoostingClassifier(),
     "Skl HistGBM": GradientBoostingClassifier(),
-    #"XGBoost": XGBClassifier(),
-    #'LightGBM': LGBMClassifier(),
-    #'CatBoost': CatBoostClassifier()
     }
 
+    mult_classifiers_wfe = {name: pipeline.make_pipeline(mult_prepro_wfe, model) for name, model in mult_models.items()}
+    tree_classifiers_wfe = {name: pipeline.make_pipeline(tree_prepro_wfe, model) for name, model in tree_models.items()}
 
-    mult_classifiers = {name: pipeline.make_pipeline(mult_prepro, model) for name, model in mult_classifiers.items()}
-    tree_classifiers = {name: pipeline.make_pipeline(tree_prepro, model) for name, model in tree_classifiers.items()}
+    models_wfe = {**mult_classifiers_wfe, **tree_classifiers_wfe}
 
-    models = {**mult_classifiers, **tree_classifiers}
-
-    #tree_classifiers["LightGBM"]
-    st.header('Model performance')
-
-    #with split
-
-    x = full_df[num_vars + cat_vars]
-    y = full_df.target
-
-    x_train, x_val, y_train, y_val = model_selection.train_test_split(x,y,test_size=0.2,stratify=y,random_state=0)
+    x_wfe = full_df[num_vars_wfe + cat_vars]
+    y_wfe = full_df.target
+    x_train, x_val, y_train, y_val = model_selection.train_test_split(x_wfe,y_wfe,test_size=0.2,stratify=y_wfe,random_state=0)
+    results = pd.DataFrame({'Model': [], 'Accuracy': [], 'Bal Acc.': []})
 
 
-    results = pd.DataFrame({'Model': [], 'Accuracy': [], 'Bal Acc.': [], 'Time': []})
+    for model_name, model in models_wfe.items():
 
-
-    for model_name, model in models.items():
-        
+        print(model_name)
         start_time = time.time()
-        model.fit(x_train,y_train)
+        model.fit(x_train, y_train)
+        
+        
         pred = model.predict(x_val)
-        total_time = time.time() - start_time
-
+        
         results = results.append({"Model":    model_name,
                                 "Accuracy": metrics.accuracy_score(y_val, pred)*100,
                                 "Bal Acc.": metrics.balanced_accuracy_score(y_val, pred)*100,
-                                "Time":     total_time},
+                                },
                                 ignore_index=True)
-                                
-
-
-    results_ord = results.sort_values(by=['Accuracy'], ascending=False, ignore_index=True)
-    results_ord.index += 1 
-    
-    st.write(results_ord.style.bar(subset=['Accuracy', 'Bal Acc.'], vmin=0, vmax=100, color='#5fba7d'))
-
-    ### stratified version
-
-    skf = model_selection.StratifiedKFold(n_splits=10, shuffle=True, random_state=0)
-
-    results = pd.DataFrame({'Model': [], 'Accuracy': [], 'Bal Acc.': [], 'Time': []})
-
-    for model_name, model in tree_classifiers.items():    
-            
-        start_time = time.time()
-        pred = model_selection.cross_val_predict(model, x, y, cv=skf)  
-        total_time = time.time() - start_time
         
-        results = results.append({"Model":    model_name,
-                                "Accuracy": metrics.accuracy_score(y, pred)*100,
-                                "Bal Acc.": metrics.balanced_accuracy_score(y, pred)*100,
-                                "Time":     total_time},
-                                ignore_index=True)
-
         results_ord = results.sort_values(by=['Accuracy'], ascending=False, ignore_index=True)
         results_ord.index += 1 
         
         clear_output(wait=True)
-    #st.write(results_ord.style.bar(subset=['Accuracy', 'Bal Acc.'], vmin=0, vmax=100, color='#5fba7d'))
+    st.write(results_ord.style.bar(subset=['Accuracy', 'Bal Acc.'], vmin=0, vmax=100, color='#5fba7d'))
 
-    st.markdown()
