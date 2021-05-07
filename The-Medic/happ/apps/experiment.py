2526ccedc33686bcc6af452a61b5c65bc40e6721
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-05-07 15:27:38 +0200

    f

--- The-Medic/happ/apps/experiment.py
+++ The-Medic/happ/apps/experiment.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-#from pandas_profiling import ProfileReport
+from pandas_profiling import ProfileReport
 #from streamlit_pandas_profiling import st_profile_report
 
 
@@ -12,23 +12,22 @@ def app():
     # Upload CSV data
     with st.header('1. Upload your CSV data'):
         uploaded_file = st.file_uploader("Upload your input CSV file", type=["csv"])
-        #st.sidebar.markdown("""
-    #[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv)
-    #""")
+        
 
-    # Pandas Profiling Report
     if uploaded_file is not None:
         @st.cache
         def load_csv():
             csv = pd.read_csv(uploaded_file)
             return csv
         df = load_csv()
-        #pr = ProfileReport(df, explorative=True)
+         pr = ProfileReport(df, explorative=True)
         st.header('**Input DataFrame**')
         st.write(df)
         st.write('---')
-        #st.header('**Pandas Profiling Report**')
-        #st_profile_report(pr)
+        st.header('**Pandas Profiling Report**')
+        st_profile_report(pr)
+            
+        
     else:
         st.info('Awaiting for CSV file to be uploaded.')
         if st.button('Press to use Example Dataset'):
@@ -39,11 +38,13 @@ def app():
                 
                 return a
             df = load_data()
-            #pr = ProfileReport(df, explorative=True)
+            pr = ProfileReport(df, explorative=True)
             st.header('**Input DataFrame**')
             st.write(df)
             st.write('---')
-            #st.header('**Pandas Profiling Report**')
-            #st_profile_report(pr)
+            st.header('**Pandas Profiling Report**')
+            st_profile_report(pr)
+            
+            
 
     
