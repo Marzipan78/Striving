346c5440f31d5893217bececd37d3ce8bea76d23
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-05-11 21:01:56 +0200

    Update sorting.py

--- Projects/Algo_Brain/sorting.py
+++ Projects/Algo_Brain/sorting.py
@@ -16,7 +16,8 @@ def merge_sort(arr):
             if left_arr[i]  < right_arr[j]:
                 arr[k] = left_arr[i]
                 i += 1
-                k += 1
+                
             else:
                 arr[k] = right_arr[j]
-                j+=1 
+                j += 1 
+            k += 1
