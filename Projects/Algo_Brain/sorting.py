1a26ea10f56718e820e87f5fb3f44a2dee755951
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-05-11 20:59:00 +0200

    Update sorting.py

--- Projects/Algo_Brain/sorting.py
+++ Projects/Algo_Brain/sorting.py
@@ -1,2 +1,14 @@
 import numpy as np
 
+def merge_sort(arr):
+    if len(arr) > 1:
+        left_arr = arr[:len(arr)//2]
+        right_arr = arr[len(arr)//2:]
+
+        merge_sort(left_arr)
+        merge_sort(right_arr)
+
+        #merge
+        i=0
+        j=0
+
