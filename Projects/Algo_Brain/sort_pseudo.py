63c61ed2bda5c8e02d3c1fb84ec30664fd7333f1
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-05-12 18:13:29 +0200

    up

--- Projects/Algo_Brain/sort_pseudo.py
+++ Projects/Algo_Brain/sort_pseudo.py
@@ -1,16 +1,20 @@
-def merge_sorta(A,p,r):
-    if p < r:
-        q = [(p+q)/2]
-        merge_sorta(A,p,q)
-        merge_sorta(A,q+1,r)
+def merge_sort(A,p,r):
+    if p < r: 
+        q = [( p + q)/ 2]
+        
+        merge_sort(A,p,q)
+        merge_sort(A,q+1,r)
 
 def merge(A,p,q,r):
-    n1 = q-p + 1
+    n1 = q - p + 1
     n2 = r - q
 
-    l = 0
-    l1 = 0 
+    l = 0  
+    l2 = 0
 
-    for i in n1:
-        l[i] = A[p + i - 1]
+    for i  in  range(1,len(n1)):
+        l[i] = A[p + i - 1 ]
+
+    for j in range(1,len(n2)):
+        R[j] = A[q + j]
         
--- Projects/Algo_Brain/sorting.py
+++ Projects/Algo_Brain/sorting.py
@@ -1,10 +1,14 @@
+
+from time import time 
 def merge_sort(arr):
     '''merge sort splits the array into two and further breaks it and then joins it later'''
 
     if len(arr) > 1:
+        # dividing the array into two
         l_arr = arr[:len(arr)//2]
         r_arr = arr[len(arr)//2:]
 
+        #performing recurssion
         merge_sort(l_arr)
         merge_sort(r_arr)
 
@@ -12,6 +16,8 @@ def merge_sort(arr):
         i = 0
         j = 0
         k = 0
+
+        
         while i < len(l_arr) and j < len(r_arr):    
             if l_arr[i]  < r_arr[j]: 
                 arr[k] = l_arr[i]
@@ -32,13 +38,17 @@ def merge_sort(arr):
             arr[k] = r_arr[j]
             j += 1
             k += 1
-import numpy as np
-#np.random.seed(0)
-#big_list = np.random.permutation(100)
+
+    else:
+        return arr 
+
+
 
 arr_test = [2,4,5,7,1,9,31,35,15,6,36,13,5,0]
-#merge_sort(big_list)
-#print(big_list)
+simple_list = [5,1,3,8,4,7,2,9,0,6]
+start = time()
+merge_sort(arr_test)
+print(arr_test)
+end = time( )
 
-import os
-os.environ['PATH']
+print("it took: ", end-start, "s")
