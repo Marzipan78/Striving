4aba2d3db5f4875c5a97da6f60c9478652ddb21c
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-05-14 12:29:42 +0200

    Update app.py

--- Projects/Soledo/app.py
+++ Projects/Soledo/app.py
@@ -19,9 +19,21 @@ def main():
     st.title("Sedora")
     st.subheader("Everything is just a click away! ")
 
-    image = Image.open("download.jpg")
-    #emo = emoji.emojize(":smiley:")
-    st.image(image,caption=f"Sedora making it possible"  ) ;
+    menu = ["Home","Analysis"]
+    #submenu = ["Plot","Prediction"]
+
+    choice = st.sidebar.selectbox("Menu",menu)
+    if choice == "Home":
+        st.subheader("Home")
+        image = Image.open("download.jpg")
+        #emo = emoji.emojize(":smiley:")
+        st.image(image,caption=f"Sedora making it possible"  ) 
+        st.markdown("### Why Sedora?")
+        st.markdown(" We are not just your average E-commerce site! You would like to know who you are getting in bed with, we understand ;)")
+        st.markdown(" On the surface we look like any other onlin shopping platform however we implement state of the art Data Analysis and Predictive models. We utilize this in order to identify Sales and to identify trends prior to them actually happening!")
+        st.markdown(" We are a powerhouse in Turkey and we would like to help you to help us to make a global impact. Expand into various ventures as our Model: Magic-Ball is very versatile.")
+
+    
     
 
 
