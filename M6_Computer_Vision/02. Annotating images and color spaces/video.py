97966236e3aa18f86433661162a13aa9923aa5d8
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-06-08 14:58:11 +0200

    capturing video

---
+++ M6_Computer_Vision/02. Annotating images and color spaces/video.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    # Our operations on the frame come here
+    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Display the resulting frame
+    cv2.imshow('frame', gray)
+    if cv2.waitKey(1) == ord('j'):
+        break
+cap.release()
+cv2.destroyAllWindows()
