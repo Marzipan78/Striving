6b280d0a6e099e5d0834f42ea6e5fba275205115
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-06-17 01:24:19 +0200

    hmm...

--- M6_Computer_Vision/04. Kernels, callbacks and thresholding/train.py
+++ M6_Computer_Vision/04. Kernels, callbacks and thresholding/train.py
@@ -20,6 +20,60 @@ from sklearn.metrics import accuracy_score
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
+
+
+def train(model, optim, criterion):
+
+    train_loss = []
+    val_loss = []
+
+    epochs = 5
+    print_every = 50
+
+    #optimizer = optim.SGD(model.parameters(), lr=0.1)
+    for e in range(epochs):
+        running_loss = 0
+        total = 0
+        correct = 0
+        print(f"Epoch: {e+1}/{epochs}")
+
+        model.train()
+        for i, (images, labels) in enumerate(iter(dh.train_transform()[1])):
+
+        
+            images.resize_(images.size()[0], 1,28,28).cuda()
+            
+            optimizer.zero_grad()
+            
+            output = model.forward(images.cuda())   # 1) Forward pass
+            loss = criterion(output.cuda(), labels.cuda()) # 2) Compute loss
+            loss.backward()                  # 3) Backward pass
+            optimizer.step()                 # 4) Update model
+            
+            running_loss += loss.item()
+
+            train_loss.append(loss)
+        
+            
+            if i % print_every == 0:
+                print(f"\tIteration: {i}\t Loss: {running_loss/print_every:.4f}")
+                running_loss = 0
+            
+
+        model.eval()
+        with torch.no_grad():
+            for i, (images, labels) in enumerate(iter(dh.test_transform()[1])):
+                images.resize_(images.size()[0], 1,28,28)
+                outputs = model(images.cuda())
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels.cuda()).sum().item()
+                #loss_val = criterion(output_val, y_val)
+                #val_loss.append(loss_val)
+        print('Accuracy of the network: %d %%' % (
+            100 * correct / total))
+
 model = Net()
 criterion = CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.01)
@@ -27,52 +81,4 @@ if torch.cuda.is_available():
     model.cuda()
     criterion.cuda()
 
-train_loss = []
-val_loss = []
-
-epochs = 5
-print_every = 50
-
-#optimizer = optim.SGD(model.parameters(), lr=0.1)
-for e in range(epochs):
-    running_loss = 0
-    total = 0
-    correct = 0
-    print(f"Epoch: {e+1}/{epochs}")
-
-    model.train()
-    for i, (images, labels) in enumerate(iter(dh.train_transform[1])):
-
-       
-        images.resize_(images.size()[0], 1,28,28).cuda()
-        
-        optimizer.zero_grad()
-        
-        output = model.forward(images.cuda())   # 1) Forward pass
-        loss = criterion(output.cuda(), labels.cuda()) # 2) Compute loss
-        loss.backward()                  # 3) Backward pass
-        optimizer.step()                 # 4) Update model
-        
-        running_loss += loss.item()
-
-        train_loss.append(loss)
-    
-        
-        if i % print_every == 0:
-            print(f"\tIteration: {i}\t Loss: {running_loss/print_every:.4f}")
-            running_loss = 0
-        
-
-    model.eval()
-    with torch.no_grad():
-        for i, (images, labels) in enumerate(iter(testloader)):
-            images.resize_(images.size()[0], 1,28,28)
-            outputs = model(images.cuda())
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels.cuda()).sum().item()
-            #loss_val = criterion(output_val, y_val)
-            #val_loss.append(loss_val)
-    print('Accuracy of the network: %d %%' % (
-        100 * correct / total))
-
+summary(model,(1, 28, 28))
