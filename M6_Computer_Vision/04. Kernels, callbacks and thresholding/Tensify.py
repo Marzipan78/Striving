2c2088968bffdbd2ae8383429a67f05ea0cd35a5
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-06-16 16:26:02 +0200

    finally converted some nums

--- M6_Computer_Vision/04. Kernels, callbacks and thresholding/Tensify.py
+++ M6_Computer_Vision/04. Kernels, callbacks and thresholding/Tensify.py
@@ -17,7 +17,7 @@ class Tensify():
     def image_reader(path,img):
         image = path + img
         numbers = cv2.imread(image,cv2.IMREAD_GRAYSCALE)
-        plt.imshow(numbers)
+        plt.imshow(numbers, cmap='gray')
         return numbers
     
     # def rotator(img):
@@ -27,26 +27,22 @@ class Tensify():
     #     plt.imshow(rotated_img)
     #     return  rotated_img       
 
-    def detector(img):
-        imgheight=img.shape[0]
-        imgwidth=img.shape[1]
-        y1 = 0
-        M = imgheight//20
-        N = imgwidth//20
-        for y in range(0,imgheight,M):
-            for x in range(0, imgwidth, N):
-                y1 = y + M
-                x1 = x + N
-                tiles = img[y:y+M,x:x+N]
+    # def detector(img):
+    #     imgheight=img.shape[0]
+    #     imgwidth=img.shape[1]
+    #     y1 = 0
+    #     M = imgheight//20
+    #     N = imgwidth//20
+    #     for y in range(0,imgheight,M):
+    #         for x in range(0, imgwidth, N):
+    #             y1 = y + M
+    #             x1 = x + N
+    #             tiles = img[y:y+M,x:x+N]
 
-                cv2.rectangle(img, (x, y), (x1, y1), (0, 255, 0))
-                cv2.imwrite("save/" + str(x) + '_' + str(y)+".png",tiles)
+    #             cv2.rectangle(img, (x, y), (x1, y1), (0, 255, 0))
+    #             cv2.imwrite("save/" + str(x) + '_' + str(y)+".png",tiles)
         
-        cv2.imwrite("digdiv.png",img)
-      
-
-
-
+    #     cv2.imwrite("digdiv.png",img)
 
     def imagify(path,img):
         
@@ -61,7 +57,7 @@ class Tensify():
         img = cv2.bitwise_not(img)
         
         fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4))
-        ax1.imshow(number);
+        ax1.imshow(number, cmap='gray');
         ax2.imshow(img);
         return img
 
