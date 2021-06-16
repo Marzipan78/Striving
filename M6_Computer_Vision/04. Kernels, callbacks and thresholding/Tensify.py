c2c2be1235249e05c5a6050c93fba21c39d9e9e6
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-06-16 15:46:48 +0200

    more updates

--- M6_Computer_Vision/04. Kernels, callbacks and thresholding/Tensify.py
+++ M6_Computer_Vision/04. Kernels, callbacks and thresholding/Tensify.py
@@ -10,6 +10,7 @@ import torch.nn.functional as F
 
 from torchvision import datasets, transforms
 from PIL import Image
+import pytesseract
 
 class Tensify():
 
@@ -25,6 +26,26 @@ class Tensify():
     #     rotated_img = img.transpose(Image.ROTATE_270)
     #     plt.imshow(rotated_img)
     #     return  rotated_img       
+
+    def detector(img):
+        imgheight=img.shape[0]
+        imgwidth=img.shape[1]
+        y1 = 0
+        M = imgheight//20
+        N = imgwidth//20
+        for y in range(0,imgheight,M):
+            for x in range(0, imgwidth, N):
+                y1 = y + M
+                x1 = x + N
+                tiles = img[y:y+M,x:x+N]
+
+                cv2.rectangle(img, (x, y), (x1, y1), (0, 255, 0))
+                cv2.imwrite("save/" + str(x) + '_' + str(y)+".png",tiles)
+        
+        cv2.imwrite("digdiv.png",img)
+      
+
+
 
 
     def imagify(path,img):
