18760084848d3ab51cf3891dfea0fc455f2801dc
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-06-30 15:56:49 +0200

    updates

--- Projects/Genre Classifier/dataprep.py
+++ Projects/Genre Classifier/dataprep.py
@@ -4,60 +4,44 @@ import librosa
 import librosa.feature
 import librosa.display
 import glob 
-import matplotlib.pyplot as plt
-import torch
-import torchvision
-from torch.utils.data import Dataset, DataLoader, dataloader, sampler
+# import matplotlib.pyplot as plt
+# import torch
+# import torchvision
+# from torch.utils.data import Dataset, DataLoader, dataloader, sampler
 
 
-class AudioDataset(Dataset):
-    def init(self,root_path):
-        super().__init__()
-        self.n_samples = len(self.all_features)
 
+def extract_features_song(self,f):
+    y,_ = librosa.load(f)
+    mfcc = librosa.feature.mfcc(y)
+    mfcc /= np.amax(np.absolute(mfcc))
 
-    def __getitem__(self, index):
-        # we want to be index like dataset[index]
-        # to get the index-th batch
-        return self.all_features[index], self.all_labels[index]
+    return np.ndarray.flatten(mfcc)[:25000]
 
+#     def generate_features_and_labels(self,root_path):
+#         self.all_features = []
+#         self.all_labels = []
 
-    def __len__(self):
-        # to retrieve the total samples by doing len(dataset)
+#         genres = ["blues", "classical", "country", "disco", "hiphop","jazz","metal","pop","reggae","rock"]
+#         for genre in genres:
+#             sound_files = glob.glob(root_path + genre + "/*.wav")
+#             print("Processing %d songs in %s genre..." % (len(sound_files), genres))
+#             for f in sound_files:
+#                 features = extract_features_song(f)
+#                 self.all_features.append(features)
+#                 self.all_labels.append(genre)
 
-        return self.n_samples 
+#     def data_splitter(self,features,labels):
+#         training_split = 0.8
+#         alldata = np.column_stack((features,labels))
 
+#         np.random.shuffle(alldata)
+#         # splitidx
 
-    def extract_features_song(self,f):
-        y,_ = librosa.load(f)
-        mfcc = librosa.feature.mfcc(y)
-        mfcc /= np.amax(np.absolute(mfcc))
+# rdir = "sampleFolder/genres/"
 
-        return np.ndarray.flatten(mfcc)[:25000]
+# dataset = AudioDataset(root_path = rdir)
+# dataloader = DataLoader(dataset,32,True)
+# samples = iter(dataloader)
+# features,labels = samples.next()
 
-    def generate_features_and_labels(self,root_path):
-        self.all_features = []
-        self.all_labels = []
-
-        genres = ["blues", "classical", "country", "disco", "hiphop","jazz","metal","pop","reggae","rock"]
-        for genre in genres:
-            sound_files = glob.glob(root_path + genre + "/*.wav")
-            print("Processing %d songs in %s genre..." % (len(sound_files), genres))
-            for f in sound_files:
-                features = extract_features_song(f)
-                self.all_features.append(features)
-                self.all_labels.append(genre)
-
-    def data_splitter(self,features,labels):
-        training_split = 0.8
-        alldata = np.column_stack((features,labels))
-
-        np.random.shuffle(alldata)
-        # splitidx
-
-rdir = "sampleFolder/genres/"
-
-dataset = AudioDataset(root_path = rdir)
-dataloader = DataLoader(dataset,32,True)
-samples = iter(dataloader)
-features,labels = samples.next()
