bb8c08abd4bbef955cdb578ae12e3992ffae84aa
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-07-07 01:21:15 +0200

    Update sel_watson.py

---
+++ Web-scraping/Selenium_projects/sel_watson.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+# path
+PATH = ("C:\Program Files (x86)\chromedriver.exe")
+driver = webdriver.Chrome(PATH)
+
+url = "http://the-internet.herokuapp.com/login"
+
+
+driver.find_element_by_xpath('//*[@id="username"]')
+# driver.find_element_by_xpath('//*[@id="password"]').send_keys("SupersecretPassword")
+# driver.find_element_by_xpath('//*[@id="login"]/button/i').click()
+
+
+driver.get(url)
