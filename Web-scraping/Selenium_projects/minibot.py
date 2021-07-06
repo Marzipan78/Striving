166c136225f455eed97be2edcb774d2f0882040f
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-07-07 01:05:35 +0200

    hit some snag

--- Web-scraping/Selenium_projects/minibot.py
+++ Web-scraping/Selenium_projects/minibot.py
@@ -1,6 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
-driver.get("https://www.amazon.de/")
+
+
+driver.get("https://www.techwithtim.net/")
 print(driver.title)
+
+
+search = driver.find_elements_by_class_name("s")
+search.send_keys("test")
+search.send_keys(Keys.RETURN) 
+
+time.sleep(5)
+
 driver.quit()
