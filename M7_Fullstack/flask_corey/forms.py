dc31d5817e6c6435a72c3dec9017b5aab2bba981
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-07-13 04:54:51 +0200

    Added Login form

--- M7_Fullstack/flask_corey/forms.py
+++ M7_Fullstack/flask_corey/forms.py
@@ -1,12 +1,29 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.fields.simple import PasswordField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.fields.core import BooleanField
+from wtforms.fields.simple import PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
     username = StringField("Username",
                             validators=[ DataRequired(), Length(min = 2, max = 20)] )
+
     email = StringField("Email",
                         validators= [DataRequired(), Email()])
+
     password = PasswordField("Password", validators=[DataRequired()])
-    confirm_password = PasswordField("Password", validators=[DataRequired()])
+
+    confirm_password = PasswordField("Password", validators=[DataRequired(), EqualTo('password')])
+
+    submit = SubmitField("Sign Up")
+
+class LoginForm(FlaskForm):
+    
+    email = StringField("Email",
+                        validators= [DataRequired(), Email()])
+
+    password = PasswordField("Password", validators=[DataRequired()])
+
+    remember = BooleanField("Remember Me")
+
+    submit = SubmitField("Login")
