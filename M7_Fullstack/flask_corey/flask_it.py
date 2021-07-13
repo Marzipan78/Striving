b952912450e6257400844abaed12987c6bfc68f8
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-07-13 05:00:01 +0200

    added routes for login and register

--- M7_Fullstack/flask_corey/flask_it.py
+++ M7_Fullstack/flask_corey/flask_it.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+
 app =  Flask(__name__)
 
 app.config["SECRET_KEY"] = 'a7d6cb865a470510e457fb52ba752369'
@@ -21,11 +23,21 @@ posts = [
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html", posts=posts)
+    return render_template("home.html", posts = posts)
 
 @app.route("/about")
 def about():
-    return render_template("about.html", title= "About")
+    return render_template("about.html", title = "About")
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template("register.html", title = "Register", form = form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title = "Login", form = form)
 
 if __name__ == "__main__":
     app.run(debug=True)
