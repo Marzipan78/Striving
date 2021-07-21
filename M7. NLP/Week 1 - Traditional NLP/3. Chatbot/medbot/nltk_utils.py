dddd1257bd8d74c0859e272a84edc3261573732f
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-07-21 14:52:57 +0200

    medbot

---
+++ M7. NLP/Week 1 - Traditional NLP/3. Chatbot/medbot/nltk_utils.py
@@ -0,0 +1,46 @@
+from re import A
+import nltk
+#nltk.download("punkt")
+from nltk.stem.porter import PorterStemmer
+import numpy as np
+
+stemmer = PorterStemmer()
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)    
+
+
+def stem(word):
+    return stemmer.stem(word.lower()) 
+
+
+
+def bag_of_words(tokenized_sentence, all_words):
+    
+    """
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bag_of_words = [0, 1, 0, 1, 0, 0, 0]
+    """
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+            
+    return(bag)
+    
+
+# a = "How long does shipping take?"
+# print(a)
+# a = tokenize(a) 
+# print(a)
+
+words = ["organize", "organizes", "organize", "organizing", "organizes", "organizing", "organized", "organizing"]
+stemmed_words = [stem(w) for w in words]
+# print(stemmed_words)
+
+sentence = ["hello", "how", "are", "you"]
+words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+bog = bag_of_words(sentence, words)
