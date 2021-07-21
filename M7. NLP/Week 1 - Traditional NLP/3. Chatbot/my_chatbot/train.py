807a79d46b59db275ae74e9f622964ac640a407e
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-07-21 12:29:45 +0200

    sorted the tags and stemmed the words

--- M7. NLP/Week 1 - Traditional NLP/3. Chatbot/my_chatbot/train.py
+++ M7. NLP/Week 1 - Traditional NLP/3. Chatbot/my_chatbot/train.py
@@ -16,4 +16,10 @@ for intent in intents['intents']:
         xy.append((w, tag))
 
 ignore_words = ["?", "!", ".", ",", ";", ":", "...", "'", "’"]
-print(all_words)
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
+#print(all_words)
+print(tags)
+
+
