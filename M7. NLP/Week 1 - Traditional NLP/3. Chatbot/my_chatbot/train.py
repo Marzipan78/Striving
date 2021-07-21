d896113e7b2aad967e301ba549bbaa5c45e6320c
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-07-21 12:34:37 +0200

    created the train data
    unzipped the tokenized words and the tags and appended them to the respective variables

--- M7. NLP/Week 1 - Traditional NLP/3. Chatbot/my_chatbot/train.py
+++ M7. NLP/Week 1 - Traditional NLP/3. Chatbot/my_chatbot/train.py
@@ -1,4 +1,5 @@
 import json
+import numpy as np
 from nltk_utils import tokenize, stem, bag_of_words
 
 with open('3. Chatbot\my_chatbot\intents.json', 'r') as f:
@@ -22,4 +23,14 @@ tags = sorted(set(tags))
 #print(all_words)
 print(tags)
 
+X_train = []
+y_train = []
+for (pattern_sentence, tag) in xy:
+    bag = bag_of_words(pattern_sentence, all_words)
+    X_train.append(bag)
 
+    label = tags.index(tag) # index of the label       
+    y_train.append(label)   # sometimes you may need to use onehot encoding, # CrossEntropyloss to be used 
+
+X_train = np.array(X_train) # convert to numpy arrays
+y_train = np.array(y_train)          
