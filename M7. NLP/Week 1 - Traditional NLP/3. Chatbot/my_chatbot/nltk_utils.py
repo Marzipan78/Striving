99b76a973f136f29fe5352f461b65397c83193e6
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-07-21 12:41:24 +0200

    updated the bag_of words

--- M7. NLP/Week 1 - Traditional NLP/3. Chatbot/my_chatbot/nltk_utils.py
+++ M7. NLP/Week 1 - Traditional NLP/3. Chatbot/my_chatbot/nltk_utils.py
@@ -2,6 +2,8 @@ from re import A
 import nltk
 #nltk.download("punkt")
 from nltk.stem.porter import PorterStemmer
+import numpy as np
+
 stemmer = PorterStemmer()
 
 def tokenize(sentence):
@@ -14,7 +16,21 @@ def stem(word):
 
 
 def bag_of_words(tokenized_sentence, all_words):
-    pass
+    
+    """
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bag_of_words = [0, 1, 0, 11, 0, 0, 0]
+    """
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    for idx,w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+            
+    return(bag)
+    
 
 # a = "How long does shipping take?"
 # print(a)
