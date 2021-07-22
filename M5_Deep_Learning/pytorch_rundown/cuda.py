50b75015e3944680bc366d7068d8a7955a4fbb98
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-07-22 14:52:13 +0200

    Create cuda.py

---
+++ M5_Deep_Learning/pytorch_rundown/cuda.py
@@ -0,0 +1,11 @@
+import torch
+import numpy as np
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    x = torch.ones(5, device=device)
+    y = torch.ones(5)
+    y = y.to(device)
+    z = x + y
+    print(z)
+    print(z.cpu().numpy()) # cpu() is a method z.to("cpu")
