db6c6b8b95e3781b7ab514125b737df0e6d3c9ef
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-07-22 15:16:11 +0200

    exploring the options
    with torch.no_grad() allows for the most operations to be undertaken

--- M5_Deep_Learning/pytorch_rundown/autograd.py
+++ M5_Deep_Learning/pytorch_rundown/autograd.py
@@ -3,13 +3,29 @@ import torch
 x =  torch.randn(3, requires_grad=True) # requires_grad=True, pytorch to save the gradient function used to compute the gradients
 print(x)
 
-y = x+2
-print(y)
+# y = x+2
+# print(y)
 
-z = y*y*2   # will return multi-dimensional tensor, therefore it is important to specify the arg in the backward() function
-#z = z.mean()    # will return one value, since z is a scalar value there is no need to specify the arg in the backward() function
-print(z)
+# z = y*y*2   # will return multi-dimensional tensor, therefore it is important to specify the arg in the backward() function
+# #z = z.mean()    # will return one value, since z is a scalar value there is no need to specify the arg in the backward() function
+# print(z)
 
-v = torch.tensor([0.1, 1.0, 0.0001], dtype=torch.float32) # scalar quant as the tchekovian multiplication requires it, the backward function is based on this.
-z.backward(v)    # dz/dx  
-print(x.grad)
+# v = torch.tensor([0.1, 1.0, 0.0001], dtype=torch.float32) # vector quant as the tchekovian multiplication requires it, the backward function is based on this.
+# z.backward(v)    # dz/dx  
+# print(x.grad)
+
+"""Ways to stop the gadient tracking"""
+
+# x.requires_grad_(False)
+x.requires_grad_(False)
+print(x)
+
+# x.detach() , creates a new tensor
+p = x.detach()
+print(p)
+
+# with torch.no_grad():  # this is a context manager, it is used to disable the gradient tracking
+
+with torch.no_grad():
+    q = x + 2
+    print(q)
