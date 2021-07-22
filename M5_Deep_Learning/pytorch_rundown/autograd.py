8d4c4ebf9b46d949ab2e436636e54787e8abe9d0
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-07-22 15:23:06 +0200

    Update autograd.py

--- M5_Deep_Learning/pytorch_rundown/autograd.py
+++ M5_Deep_Learning/pytorch_rundown/autograd.py
@@ -34,9 +34,19 @@ with torch.no_grad():
 
 weights = torch.ones(4, requires_grad=True)
 
-for epoch in range(2):
+for epoch in range(6):
     model_output = (weights * 3).sum()
     
     model_output.backward()
     
     print(weights.grad)
+
+    weights.grad.zero_()  #  to avoid the gradient from accumulating
+
+"""Using the Optimizer
+
+optimizer = torch.optim.SGD(weights, lr=0.01)
+optimizer.step()
+optimizer.zero_grad()  # this is to clear the gradients, avoiding the accumulation of the gradients
+
+"""
