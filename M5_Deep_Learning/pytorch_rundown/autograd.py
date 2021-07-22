5359dd6ce715f1cb0db3d8fb2cdd638bbcb45d71
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-07-22 15:10:14 +0200

    adding v as the scalar

--- M5_Deep_Learning/pytorch_rundown/autograd.py
+++ M5_Deep_Learning/pytorch_rundown/autograd.py
@@ -10,5 +10,6 @@ z = y*y*2   # will return multi-dimensional tensor, therefore it is important to
 #z = z.mean()    # will return one value, since z is a scalar value there is no need to specify the arg in the backward() function
 print(z)
 
-z.backward()    # dz/dx  
+v = torch.tensor([0.1, 1.0, 0.0001], dtype=torch.float32) # scalar quant as the tchekovian multiplication requires it, the backward function is based on this.
+z.backward(v)    # dz/dx  
 print(x.grad)
