6abdd24b8441d0c21d8ad731e34a4e9a5d745e4f
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-07-22 14:53:45 +0200

    Update tensorbasics.py

--- M5_Deep_Learning/pytorch_rundown/tensorbasics.py
+++ M5_Deep_Learning/pytorch_rundown/tensorbasics.py
@@ -2,11 +2,14 @@ import torch
 import numpy as np
 
 a = np.ones(5)
-print(a)
+# print(a)
 
 b = torch.from_numpy(a)
-print(b)
+# print(b)
 
 a +=1
-print(a)
-print(b)
+# print(a)
+# print(b)
+
+x = torch.ones(5,requires_grad=True)
+print(x)
