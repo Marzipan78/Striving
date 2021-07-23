195cf88c61000188cca2d511dad952657608f352
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-07-23 12:56:41 +0200

    overriding parent init

--- OOP/p3.py
+++ OOP/p3.py
@@ -5,16 +5,35 @@
 -> a child class can also add additional methods
 """
 
-class Employee:   #parent class
+class Employee:   # parent class
     
-    def __init__(self, name, age) -> None:
+    def __init__(self, name, age, salary):
         self.name = name
         self.age = age
+        self.salary = salary
+
+    def work(self):
+        print(f"{self.name}, is working...")        
 
 
 class SoftwareEngineer(Employee):   # child class 
-    pass
+    
+    def __init__(self, name, age, salary, level):
+        super().__init__(name, age, salary)
+        self.level = level
 
+    
+        
 
 class Designer(Employee):   # child class   
     pass
+
+
+#se = SoftwareEngineer() # TypeError EXPECTED: __init__() missing 2 required positional arguments: 'name' and 'age',
+                        # as it inhereted from Employee
+
+se1 = SoftwareEngineer("John", 30, 5000, "Junior")	
+print(se1.level)
+
+d = Designer("Jane", 30, 6000)
+d.work()
