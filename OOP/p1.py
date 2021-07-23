950e752cc1b87b7a1f88cd800afb71e6252aa74e
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-07-23 10:43:47 +0200

    OOP
    Solidfying OOP knowledge

---
+++ OOP/p1.py
@@ -0,0 +1,19 @@
+"""For SOFTWARE ENGINEER"""
+
+# position, name, age, level, salary 
+from typing_extensions import ParamSpecArgs
+
+
+se1 = ["Software Engineer", "Sara", 20, "Junior", 4000]
+se1 = ["Software Engineer", "Felix", 27, "Senior", 6000]
+
+# Issues: not flexible, no error checking
+# Solution: Use a class
+
+# Class
+class SoftwareEngineer:
+    pass 
+
+
+#instance
+se1 = SoftwareEngineer() 
