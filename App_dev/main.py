78a296501ac0fa0a35118de8fe4238ca50b2f96e
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-08-02 11:29:26 +0200

    Update main.py

--- App_dev/main.py
+++ App_dev/main.py
@@ -1,13 +1,13 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.label import BoxLayout
 
 kivy.require('1.9.0')
 
 class RandomNumber(App):
 
     def build(self):
-        return Label(text="Random Number")
+        return BoxLayout()
 
 
 randomnumber = RandomNumber()
